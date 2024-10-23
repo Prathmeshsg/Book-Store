3a03368d1705ea488bb27aba14b4adb9b90666f1
Prathmesh Gandhale <125843181+Shadowgit98@users.noreply.github.com>
2024-10-23 13:39:17 +0530

    first commit

---
+++ frontend/src/routers/router.jsx
@@ -0,0 +1,30 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
+import Home from "../pages/home/Home";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        children: [
+            {
+                path: "/",
+                element: <Home />
+            },
+            {
+                path: "/about",
+                element: <h1>About</h1>
+            },
+            {
+                path: "/orders",
+                element: <h1>Orders</h1>
+            },
+            {
+                path: "/profile",
+                element: <h1>Profile</h1>
+            },
+        ],
+    },
+]);
+
+export default router;
